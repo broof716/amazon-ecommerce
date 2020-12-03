c477a5906d85771f4c4b20e71014c85e5002957c
Bryan Roof <broof716@gmail.com>
2020-12-03 15:53:48 -0700

    created MessageBox & LoadingBox

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 
 export default function HomeScreen() {
@@ -27,7 +29,7 @@ return (
      <LoadingBox></LoadingBox>
     :
     error ?
-    <MessageBox>{error}</MessageBox>
+    <MessageBox variant="danger">{error}</MessageBox>
     : (      
     <div className="row center">
       {products.map(product => (
