7b16b202d5fdadb017be436c95dd2348b0ccf137
Bryan Roof <broof716@gmail.com>
2020-12-05 18:50:08 -0700

    added product details

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,16 @@
-const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } = require("../constants/productConstants");
+const { 
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS, 
+  PRODUCT_LIST_FAIL, 
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
+} = require("../constants/productConstants");
 
-export const productListReducer = (state = {loading: true, products: [] }, action) => {
+export const productListReducer = (
+  state = {loading: true, products: [] }, 
+  action
+  ) => {
   switch(action.type){
     case PRODUCT_LIST_REQUEST:
       return {loading: true};
@@ -10,5 +20,18 @@ export const productListReducer = (state = {loading: true, products: [] }, actio
       return {loading: false, error: action.payload};
     default:
       return state;
+  }
+};
+
+export const productDetailsReducer = (state = { product:{}, loading: true}, action) =>{
+  switch (action.type){
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true};
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state
   }
 }
