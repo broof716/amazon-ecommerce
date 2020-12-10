10ee10a4e9a36b044cdbaa84ff01053b9583da46
Bryan Roof <broof716@gmail.com>
2020-12-09 18:07:43 -0700

    created an api to send to frontend

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
-import Product from '../models/product.Model.js';
+import Product from '../models/productModel.js';
 
 const productRouter = express.Router();
+
+productRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
 
 productRouter.get(
   '/seed',
@@ -11,6 +19,18 @@ productRouter.get(
     // await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
     res.send({products: createdProducts});
+  })
+);
+
+productRouter.get(
+  '/:id',
+    expressAsyncHandler(async (req, res) => {
+    const product =await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({message:'Product Not Found'})
+    }
   })
 );
 
