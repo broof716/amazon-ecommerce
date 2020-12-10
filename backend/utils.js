37f4e703e673c7661f34f6a603fe373c3170f715
Bryan Roof <broof716@gmail.com>
2020-12-10 14:39:20 -0700

    installed dotenv package

--- backend/utils.js
+++ backend/utils.js
@@ -1,10 +1,16 @@
-import { JsonWebTokenError } from "jsonwebtoken"
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export const generateToken = (user) => {
-  return JsonWebTokenError.sign({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-  });
+  return JsonWebTokenError.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    } 
+  );
 };
