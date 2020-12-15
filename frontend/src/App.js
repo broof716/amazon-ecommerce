0c0c5822f35fda10ce1f1faf9db8146f780a28b8
Bryan Roof <broof716@gmail.com>
2020-12-14 17:15:32 -0700

    implimented signout dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -13,7 +14,10 @@ function App() {
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
-
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -28,23 +32,29 @@ function App() {
               <span className="badge">{cartItems.length}</span>
             )}
           </Link>
-          {
-            userInfo ? (
-              <Link to="#">{userInfo.name}</Link>
-            ) :
-            (
-              <Link to="/signin">Sign In</Link> 
-            )
-          }
+          {userInfo ? (
+            <div className="dropdown">
+              <Link to="#">
+                {userInfo.name} <i className="fa fa-caret-down"></i>{' '} 
+              </Link>
+              <ul className="dropdown-content">
+                <Link to="#signout" onClick={signoutHandler}>
+                  Sign Out
+                </Link>
+              </ul>
+            </div>
+          ) : (
+            <Link to="/signin">Sign In</Link> 
+          )}
         </div>
       </header>
-        <main>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/signin" component={SigninScreen}></Route>
-          <Route path="/" component={HomeScreen} exact></Route>
-        </main>
-        <footer className="row center">All rights reserved</footer>
+      <main>
+        <Route path="/cart/:id?" component={CartScreen}></Route>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/signin" component={SigninScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+      </main>
+      <footer className="row center">All rights reserved</footer>
     </div>
   </BrowserRouter>
   );
