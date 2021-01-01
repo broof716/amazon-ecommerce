83f9370b28675a929f527e6316c6be6d563ea1f2
Bryan Roof <broof716@gmail.com>
2021-01-01 16:00:37 -0700

    added paid status on checkout screen

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -47,6 +47,8 @@ export const orderPayReducer = (state = {}, action) => {
       return { loading: false, success: true };
     case ORDER_PAY_FAIL:
       return { loading: false, error: action.payload};
+    case ORDER_PAY_RESET:
+      return {};
     default:
       return state;
   }
