9797aed2b728597c8755613630ea17cc2a54b778
Bryan Roof <broof716@gmail.com>
2021-01-02 12:09:50 -0700

    created a table header in OrderHistoryScreen.js

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function OrderHistoryScreen() {
+  const orderMineList = useSelector(state => state.orderMineList);
+  const { loading, error, orders } = orderMineList;
   return (
     <div>
-      <h1>Order History</h1>
-      
+      <h1>Order History</h1>   
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+        </table>
+      )}
     </div>
-  )
+  );
 }
