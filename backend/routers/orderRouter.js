0b148cc23bfef4c0e9c2c510e8f286d40bb69a15
Bryan Roof <broof716@gmail.com>
2021-01-19 17:06:05 -0700

    created new route in orderRouter to deliver an order

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -101,6 +101,7 @@ orderRouter.delete(
 orderRouter.put(
   '/:id/deliver',
   isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
     if (order) {
